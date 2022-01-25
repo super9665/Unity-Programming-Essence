a7885cd72435c6bf290c1a8e1d7342f54ca3c9f6
super9665 <wlsghks6675@gmail.com>
2022-01-25 12:55:42 +0900

    test for build

---
+++ 02/3D Demo/Assets/CubeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+
+    private Rigidbody cubeRigidbody;
+    float speed = 8f;
+    float jumpCooltime;
+    void Start()
+    {
+        cubeRigidbody = GetComponent<Rigidbody>();
+        jumpCooltime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpCooltime += Time.deltaTime;
+
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.Space) && jumpCooltime >= 0.5) {
+            cubeRigidbody.AddForce(0f, 5000f, 0f);
+            jumpCooltime = 0;
+        }
+        Vector3 direction = getNormalVector(xInput, 0, zInput) * speed;
+        cubeRigidbody.velocity = direction;
+    }
+
+    Vector3 getNormalVector(float x, float y, float z) {
+        Vector3 normalVector = new Vector3(x, y, z).normalized;
+        return (normalVector);
+    }
+}
---
+++ 02/3D Demo/Assets/cameraMoving.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMoving : MonoBehaviour
+{
+    private Transform target;
+    void Start()
+    {
+        target = FindObjectOfType<CubeScript>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + new Vector3(0f, 30f, 0f);
+    }
+}
